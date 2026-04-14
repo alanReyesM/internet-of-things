672cd1d5ff889054fd3a1d7516abed8741cc2488
Alan R. <alansebas2104@gmail.com>
2026-04-14 16:29:11 -0500

    models.py weather

--- iot_app/weather_station/models.py
+++ iot_app/weather_station/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 #
 # Create your models here. se crean singularizados 
-#class Sensor(models.Model):
-#name= models.CharField(max_length=50)
+class Sensor(models.Model):
+    name=models.CharField(max_length=50)
+    humidity = models.FloatField(default=0)
